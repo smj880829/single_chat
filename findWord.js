a028856128c0c5194714980e24ff2888d0176da4
unknown <>
2016-07-27 18:32:37 +0900

    asd

--- findWord.js
+++ findWord.js
@@ -1,49 +1,60 @@
 var request = require('request')
 
 function findWord(word,callback){
+
+
     var docs = {};
     docs.word = word;
 
     var url = 'http://dic.daum.net/search.do?q='
 
-  docs.mean = request(url+word, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    //console.log(body) // Show the HTML for the Google homepage.
-    var temp = body.split('<ul class=\"list_search\">')
+    var options = {
+    url: url+word,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
 
-    var flg = 1;
-    var j = 0;
-    var wow = "";
+  function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var temp = body.split('<ul class=\"list_search\">')
 
-    if(temp.length > 1){
-      var temp2 = temp[1].split('</ul>')
+        var flg = 1;
+        var j = 0;
+        var wow = "";
 
-      while(j < temp2[0].length)
-      {
-              if (temp2[0].charAt(j) == '<'){
-                  flg = 0;
-              }
-              else if( temp2[0].charAt(j) == '>'){
-                flg = 1;
-                j = j+1;
-                continue
-              }
+        if(temp.length > 1){
+          var temp2 = temp[1].split('</ul>')
 
-              if(flg == 0){
+          while(j < temp2[0].length)
+          {
+                  if (temp2[0].charAt(j) == '<'){
+                      flg = 0;
+                  }
+                  else if( temp2[0].charAt(j) == '>'){
+                    flg = 1;
+                    j = j+1;
+                    continue
+                  }
+
+                  if(flg == 0){
+                      j = j + 1;
+                      continue
+                  }
+                  else if( flg == 1 && temp2[0].charAt(j) != '\t' && temp2[0].charAt(j) != '\n'){
+                      wow = wow + temp2[0].charAt(j);
+                  }
+
                   j = j + 1;
-                  continue
-              }
-              else if( flg == 1 && temp2[0].charAt(j) != '\t' && temp2[0].charAt(j) != '\n'){
-                  wow = wow + temp2[0].charAt(j);
-              }
-
-              j = j + 1;
+          }
+          docs.mean = wow;
+        }
       }
-      return  callback(wow)
     }
-    }
-  })
-    return callback(docs)
+
+    request(options, callback);
+
+    callback(docs)
 }
 
 exports.findWord = findWord;
