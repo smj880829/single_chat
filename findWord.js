982ebc91c451afb9302b07b4f70713e745a80d6a
unknown <>
2016-07-27 18:50:34 +0900

    asd

--- findWord.js
+++ findWord.js
@@ -2,59 +2,49 @@ var request = require('request')
 
 function findWord(word,callback){
 
-
     var docs = {};
     docs.word = word;
 
     var url = 'http://dic.daum.net/search.do?q='
 
-    var options = {
-    url: url+word,
-    headers: {
-      'User-Agent': 'request'
-    }
-  };
+  request(url+word, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    //console.log(body) // Show the HTML for the Google homepage.
+    var temp = body.split('<ul class=\"list_search\">')
 
-  function callback(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var temp = body.split('<ul class=\"list_search\">')
+    var flg = 1;
+    var j = 0;
+    var wow = "";
 
-        var flg = 1;
-        var j = 0;
-        var wow = "";
+    if(temp.length > 1){
+      var temp2 = temp[1].split('</ul>')
 
-        if(temp.length > 1){
-          var temp2 = temp[1].split('</ul>')
+      while(j < temp2[0].length)
+      {
+              if (temp2[0].charAt(j) == '<'){
+                  flg = 0;
+              }
+              else if( temp2[0].charAt(j) == '>'){
+                flg = 1;
+                j = j+1;
+                continue
+              }
 
-          while(j < temp2[0].length)
-          {
-                  if (temp2[0].charAt(j) == '<'){
-                      flg = 0;
-                  }
-                  else if( temp2[0].charAt(j) == '>'){
-                    flg = 1;
-                    j = j+1;
-                    continue
-                  }
-
-                  if(flg == 0){
-                      j = j + 1;
-                      continue
-                  }
-                  else if( flg == 1 && temp2[0].charAt(j) != '\t' && temp2[0].charAt(j) != '\n'){
-                      wow = wow + temp2[0].charAt(j);
-                  }
-
+              if(flg == 0){
                   j = j + 1;
-          }
-          docs.mean = wow;
-        }
+                  continue
+              }
+              else if( flg == 1 && temp2[0].charAt(j) != '\t' && temp2[0].charAt(j) != '\n'){
+                  wow = wow + temp2[0].charAt(j);
+              }
+
+              j = j + 1;
       }
+      docs.mean = wow;
     }
-
-    request(options, callback);
-
-    callback(docs)
+    }
+  })
+    return callback(docs)
 }
 
 exports.findWord = findWord;
--- mysocketio.js
+++ mysocketio.js
@@ -1,7 +1,7 @@
 var io =  require('socket.io')();
 var db = require('./MongoConnector/DAO')
 var conf = require('./MongoConnector/Conf')
-var request = require('request')
+var fw = require('./findWord')
 
 module.exports = function(options) {
   io.attach(options,{origins:conf.ip +':* http://' + conf.ip +':*'});
@@ -54,53 +54,17 @@ io.on('connection', function (socket) {
       var result = new Array();
       for(var i in words)
       {
-            var docs = {};
-            docs.word = words[i];
+        fw.findWord(words[i],function(re){
+          result.push(re );
+        })
+      }
 
-            var url = 'http://dic.daum.net/search.do?q='
-
-          request(url+words[i], function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            //console.log(body) // Show the HTML for the Google homepage.
-            var temp = body.split('<ul class=\"list_search\">')
-
-            var flg = 1;
-            var j = 0;
-            var wow = "";
-            console.log(temp[0])
-            if(temp.length > 1){
-                          var temp2 = temp[1].split('</ul>')
-
-                          while(j < temp2[0].length)
-                          {
-                                  if (temp2[0].charAt(j) == '<'){
-                                      flg = 0;
-                                  }
-                                  else if( temp2[0].charAt(j) == '>'){
-                                    flg = 1;
-                                    j = j+1;
-                                    continue
-                                  }
-
-                                  if(flg == 0){
-                                      j = j + 1;
-                                      continue
-                                  }
-                                  else if( flg == 1 && temp2[0].charAt(j) != '\t' && temp2[0].charAt(j) != '\n'){
-                                      wow = wow + temp2[0].charAt(j);
-                                  }
-                                  j = j + 1;
-                          }
-            }
-        }
-      result.push(docs );
-    })
-  }
       for(var i in result)
       {
         console.log(result[i].word);
         console.log(result[i].mean);
       }
+
       socket.emit('word_result', result);
-    });
+  });
 })
